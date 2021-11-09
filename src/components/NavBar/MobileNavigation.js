aa4d3fb3d878d176161c0cfc7f4b003d4afb3d0a
OnTheRocks <nathan_huber@yahoo.com>
2021-11-08 23:03:08 -0800

    Working on Hamburger menu

--- src/components/NavBar/MobileNavigation.js
+++ src/components/NavBar/MobileNavigation.js
@@ -1,10 +1,16 @@
 import NavLinks from "./NavLinks";
 import classes from './NavBar.module.css'
+import {ImMenu} from 'react-icons/im'
+import { useState } from "react";
 
 const MobileNavication = () => {
+
+  const [open, setOpen] = useState(false)
+
   return ( 
     <nav className={classes.MobileNavigation}>
-      <NavLinks />
+      <ImMenu className={classes.Hamburger} size='40px' color='white' onClick={() => setOpen(!open)} />
+      {open && <NavLinks />}
     </nav>
    );
 }
